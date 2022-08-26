de5dc1a87b9ffc313886aa07d5db55cf1375af83
camora1990 <moorales.camilo@gmail.com>
2022-08-26 15:57:49 -0500

    [add] repository Date

--- src/main/java/com/sofka/agendamiento/repository/DateRespository.java
+++ src/main/java/com/sofka/agendamiento/repository/DateRespository.java
@@ -1,2 +1,9 @@
-package com.sofka.agendamiento.repository;public interface DateRespository {
+package com.sofka.agendamiento.repository;
+
+import com.sofka.agendamiento.collections.Date;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DateRespository extends ReactiveMongoRepository<Date, String> {
 }
