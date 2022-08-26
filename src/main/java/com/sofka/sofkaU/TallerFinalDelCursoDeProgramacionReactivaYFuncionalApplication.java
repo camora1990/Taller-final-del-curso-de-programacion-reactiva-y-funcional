78276e8d6ff0552303d98f1add702426f0db62b4
camora1990 <moorales.camilo@gmail.com>
2022-08-26 09:46:51 -0500

    first commit

---
+++ src/main/java/com/sofka/sofkaU/TallerFinalDelCursoDeProgramacionReactivaYFuncionalApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.sofkaU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerFinalDelCursoDeProgramacionReactivaYFuncionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerFinalDelCursoDeProgramacionReactivaYFuncionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofka/sofkaU/TallerFinalDelCursoDeProgramacionReactivaYFuncionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.sofkaU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerFinalDelCursoDeProgramacionReactivaYFuncionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
